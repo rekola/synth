40eced30d2d2888df114e8ee9cd49e53aaead636
Mikael Rekola <mikael.rekola@gmail.com>
2021-04-08 19:10:11 +0300

    separate instruments from the track

--- Instrument.h
+++ Instrument.h
@@ -18,33 +18,39 @@ public:
   float getFcut() const { return fcut; }
   float getFres() const { return fres; }
   unsigned char getFlags() const { return flags; }
+  bool getSolo() const { return solo; }
   
   int getAttack() const { return a; }
   int getDecay() const { return d; }
   float getSustain() const { return s; }
   int getRelease() const { return r; }
   
-  void setDetune(float _detune) { detune = _detune; }
-  void setVolume(float _volume) { volume = _volume; }
-  void setPan(float _pan) { pan = _pan; }
-  void setFilter(float _fcut, float _fres) { fcut = _fcut; fres = _fres; }
+  void setDetune(int _detune) { detune = (_detune - 127) / 512.0; }
+  void setVolume(int _volume) { volume = _volume / 128.0f; }
+  void setPan(int _pan) { pan = _pan / 255.0; }
   void setFlags(unsigned char _flags) { flags = _flags; }
+  void setSolo(bool s) {solo = s; }
     
-  void setADSR(int _a, int _d, float _s, int _r) {
-    a = _a;
-    d = _d;
-    s = _s;
-    r = _r;
+  void setFilter(int _fcut, int _fres) {
+    fcut = _fcut / 255.0f;
+    fres = _fres / 63.0f;
   }
 
+  void setADSR(int _a, int _d, int _s, int _r) {
+    a = _a * 44100 * 5 / 255;
+    d = _d * 44100 * 5 / 255;
+    s = _s / 255.0f;
+    r = _r * 44100 * 5 / 255;
+  }
   
 protected:
   int a = 0, d = 0, r = 0;
   float s = 1.0;
-  float detune = 0, volume = 1.0;
-  float pan = 0;
+  float detune = 0, volume = 1.0f;
+  float pan = 0.5f;
   float fcut = 1.0, fres = 0.0;
   unsigned char flags = 0;
+  bool solo = false;
 };
 
 #endif
--- Synth.cpp
+++ Synth.cpp
@@ -24,6 +24,148 @@ Synth::Synth(int samplerate, unsigned char *track) {
     a *= k;
   }
 
+  // string drone
+  auto i0 = make_unique<BasicInstrument>(WaveformType::SAW);
+  i0->setADSR(255, 64, 63, 0);
+  i0->setVolume(20);
+  i0->setFlags(HPFILTER);
+  i0->setDetune(127);
+  i0->setPan(10);
+  i0->setFilter(0, 5);
+  instruments.push_back(move(i0));
+
+  // string drone
+  auto i1 = make_unique<BasicInstrument>(WaveformType::SAW);
+  i1->setADSR(255, 64, 63, 0);
+  i1->setVolume(20);
+  i1->setFlags(HPFILTER);
+  i1->setDetune(120);
+  i1->setPan(127);
+  i1->setFilter(0, 5);
+  instruments.push_back(move(i1));
+
+  // string drone
+  auto i2 = make_unique<BasicInstrument>(WaveformType::SAW);
+  i2->setADSR(255, 64, 63, 0);
+  i2->setVolume(20);
+  i2->setFlags(HPFILTER);
+  i2->setDetune(134);
+  i2->setPan(247);
+  i2->setFilter(0, 5);
+  instruments.push_back(move(i2));
+
+  // bass drum 
+  auto i3 = make_unique<BasicInstrument>(WaveformType::SINE);
+  i3->setADSR(0, 15, 0, 0);
+  i3->setVolume(200);
+  i3->setDetune(130);
+  i3->setPan(127);
+  i3->setFilter(255, 0);
+  instruments.push_back(move(i3));
+
+  // hihat (closed)
+  auto i4 = make_unique<BasicInstrument>(WaveformType::NOISE2);
+  i4->setADSR(0, 8, 0, 0);
+  i4->setVolume(64);
+  i4->setFlags(DELAYTRACK);
+  i4->setDetune(127);
+  i4->setPan(127);
+  i4->setFilter(190, 128);
+  instruments.push_back(move(i4));
+
+  // hihat (open)
+  auto i5 = make_unique<BasicInstrument>(WaveformType::NOISE2);
+  i5->setADSR(0, 13, 0, 0);
+  i5->setVolume(30);
+  i5->setDetune(127);
+  i5->setPan(127);
+  i5->setFilter(255, 0);
+  instruments.push_back(move(i5));
+
+  // unused
+  auto i6 = make_unique<BasicInstrument>(WaveformType::SAW);
+  i6->setADSR(0, 25, 0, 0);
+  i6->setVolume(128);
+  i6->setDetune(127);
+  i6->setPan(127);
+  i6->setFilter(255, 0);
+  instruments.push_back(move(i6));
+
+  // bass
+  auto i7 = make_unique<BasicInstrument>(WaveformType::SQUARE);
+  i7->setADSR(0, 15, 0, 0);
+  i7->setVolume(40);
+  i7->setDetune(125);
+  i7->setPan(64);
+  i7->setFilter(200, 20);
+  instruments.push_back(move(i7));
+
+  // hihat (closed)
+  auto i8 = make_unique<BasicInstrument>(WaveformType::NOISE);
+  i8->setADSR(0, 3, 0, 0);
+  i8->setVolume(63);
+  i8->setDetune(127);
+  i8->setPan(217);
+  i8->setFilter(255, 0);  
+  instruments.push_back(move(i8));
+
+  // snare like
+  auto i9 = make_unique<BasicInstrument>(WaveformType::NOISE);
+  i9->setADSR(0, 15, 0, 0);
+  i9->setVolume(40);
+  i9->setFlags(DELAYTRACK);
+  i9->setDetune(127);
+  i9->setPan(127);
+  i9->setFilter(255, 0);
+  instruments.push_back(move(i9));
+
+  // bass
+  auto i10 = make_unique<BasicInstrument>(WaveformType::SAW);
+  i10->setADSR(0, 30, 0, 0);
+  i10->setVolume(60);
+  i10->setFlags(DELAYTRACK);
+  i10->setDetune(127);
+  i10->setPan(127);
+  i10->setFilter(100, 0);
+  instruments.push_back(move(i10));
+
+  // bass
+  auto i11 = make_unique<BasicInstrument>(WaveformType::SAW);
+  i11->setADSR(0, 20, 0, 0);
+  i11->setVolume(128);
+  i11->setFlags(DELAYTRACK);
+  i11->setDetune(127);
+  i11->setPan(128);
+  i11->setFilter(63, 128);
+  instruments.push_back(move(i11));
+
+  // bass
+  auto i12 = make_unique<BasicInstrument>(WaveformType::SQUARE);
+  i12->setADSR(0, 14, 0, 0);
+  i12->setVolume(40);
+  i12->setDetune(129);
+  i12->setPan(190);
+  i12->setFilter(200, 20);
+  instruments.push_back(move(i12));
+
+  // bass drum
+  auto i13 = make_unique<BasicInstrument>(WaveformType::SINE);
+  i13->setADSR(0, 8, 0, 0);
+  i13->setVolume(200);
+  i13->setDetune(127);
+  i13->setPan(127);
+  i13->setFilter(244, 0);
+  instruments.push_back(move(i13));
+
+  // snare like
+  auto i14 = make_unique<BasicInstrument>(WaveformType::NOISE);
+  i14->setADSR(0, 5, 0, 0);
+  i14->setVolume(240);
+  i14->setDetune(127);
+  i14->setPan(37);
+  i14->setFilter(150, 255);
+  instruments.push_back(move(i14));
+
   bpm = *track++;
   mastervol = (float)(*track++) / 127;
   delay1 = (int)(MAXDELAYSAMPLES * ((float)(*track++) / 255));
@@ -34,35 +176,15 @@ Synth::Synth(int samplerate, unsigned char *track) {
   delaymix2 = (float)(*track++) / 255;
   
   int ptrncnt = *track++;
+  
   for (int i = 0; i < ptrncnt; i++) {
-    int instrument_id = (int)instruments.size();
-    
-    WaveformType type = (WaveformType)*track++;
-    int a = (*track++) * 44100 * 5 / 255;
-    int d = (*track++) * 44100 * 5 / 255;
-    float s = (float)(*track++) / 255;
-    int r = (*track++) * 44100 * 5 / 255;
-    float vol = (float)(*track++) / 128;
-    unsigned char flags = *track++;
-    float detune = (float)((*track++) - 127) / 512;
-    float pan = (float)(*track++) / 255;
-    float fcut = (float)(*track++) / 255;
-    float fres = (float)(*track++) / 63;
+    int instrument_id = *track++;
 
-    auto instrument = make_unique<BasicInstrument>(type);
-    instrument->setADSR(a, d, s,r);
-    instrument->setVolume(vol);
-    instrument->setDetune(detune);
-    instrument->setPan(pan);
-    instrument->setFilter(fcut, fres);
-    instrument->setFlags(flags);
-    instruments.push_back(move(instrument));
-    						   
     Channel pattern;
     pattern.instrument_id = instrument_id;
     
     while (1) {
-      unsigned char val = *track++;
+      int val = *track++;
       if (val == 255) break;
       pattern.addNote(val);
     }
@@ -91,6 +213,11 @@ Synth::Synth(int samplerate, unsigned char *track) {
 
 void
 Synth::play(float * out, size_t frames) {
+  int solo_instrument = -1;
+  for (size_t i = 0; i < instruments.size(); i++) {
+    if (instruments[i]->getSolo()) solo_instrument = i;
+  }
+  
   for (int i = 0; i < frames; i++) {
     float left = 0, right = 0;
     
@@ -124,6 +251,8 @@ Synth::play(float * out, size_t frames) {
       ss = pattern.filtersample(ss, *instrument);
 
       ss *= instrument->getVolume() * adsrvol * gvol;
+      if (solo_instrument != -1 && pattern.instrument_id != solo_instrument) ss = 0;
+      
       if (pattern.acc) ss *= ACCENTAMT;
       if (ss > 1.0) ss = 1.0;
       else if (ss < -1.0) ss = -1.0;
