55cbead59a818ecb6448652cc4aceae9f2e38315
Mikael Rekola <mikael.rekola@gmail.com>
2021-04-10 18:36:12 +0300

    add more methods

--- SampleData.h
+++ SampleData.h
@@ -1,11 +1,23 @@
 #ifndef _SAMPLEDATA_H_
 #define _SAMPLEDATA_H_
 
+#include <cstring>
+
 class SampleData {
  public:
- SampleData(size_t _frames) : frames(_frames) {
-   _data = new float[2 * frames];
+  SampleData() : frames(0), _data(0) { }
+  SampleData(size_t _frames) : frames(_frames) {
+    _data = new float[2 * frames];
+    memset(_data, 0, 2 * frames * sizeof(float));
   }
+
+  SampleData(const SampleData & other) {
+    frames = other.size();
+    _data = new float[2 * frames];
+    memcpy(_data, other.data(), 2 * frames * sizeof(float));
+  }
+  const SampleData & operator=(const SampleData & other) = delete;
+  
   ~SampleData() {
     delete[] _data;
   }
@@ -13,9 +25,25 @@ class SampleData {
   float * data() { return _data; }
   const float * data() const { return _data; }
 
+  void clear() {
+    delete[] _data;
+    _data = 0;
+    frames = 0;
+  }
   size_t size() const { return frames; }
+
+  void append(const SampleData & other) {
+    if (!other.size()) return;
     
- private:
+    float * new_data = new float[2 * (size() + other.size())];
+    if (frames) memcpy(new_data, _data, 2 * frames * sizeof(float));
+    delete[] _data;
+    _data = new_data;
+    memcpy(_data + 2 * frames, other.data(), 2 * other.size() * sizeof(float));
+    frames += other.size();
+  }
+    
+private:
   size_t frames;
   float * _data;
 };
