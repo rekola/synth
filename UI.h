fc3c6040677fe5809992bcfc134aa3763b8d538d
Mikael Rekola <mikael.rekola@gmail.com>
2021-04-12 02:03:09 +0300

    make UI an UIElement

--- UI.h
+++ UI.h
@@ -1,12 +1,13 @@
 #ifndef _UI_H_
 #define _UI_H_
 
+#include "UIElement.h"
+
 #include <string>
 
-class UI {
+class UI : public UIElement {
  public:
   explicit UI() { }
-  virtual ~UI() { }
 
   virtual void setStatus(const std::string & s) = 0;
 };
