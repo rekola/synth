d634f1a7c3edfc5e85663b6bfed7629298daf2bd
Mikael Rekola <mikael.rekola@gmail.com>
2021-04-21 20:54:43 +0300

    add mouse coordinates

--- UIInput.h
+++ UIInput.h
@@ -3,11 +3,13 @@
 
 class UIInput {
  public:
-  UIInput(size_t _seqnum, int _id, bool _alt, bool _shift, bool _ctrl)
-    : seqnum(_seqnum), id(_id), alt(_alt), shift(_shift), ctrl(_ctrl) { }
+  UIInput(size_t _seqnum, int _id, int _y, int _x, bool _alt, bool _shift, bool _ctrl)
+    : seqnum(_seqnum), id(_id), y(_y), x(_x), alt(_alt), shift(_shift), ctrl(_ctrl) { }
 
   size_t getSeqnum() const { return seqnum; }
   int getId() const { return id; }
+  int getY() const { return y; }
+  int getX() const { return x; }
   bool hasAlt() const { return alt; }
   bool hasShift() const { return shift; }
   bool hasCtrl() const { return ctrl; }
@@ -15,6 +17,7 @@ class UIInput {
  private:
   size_t seqnum;
   int id;
+  int y, x;
   bool alt, shift, ctrl;
 };
 
