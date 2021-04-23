b0aac6d363c147c7fd7a210474ed6609d664e7b0
Mikael Rekola <mikael.rekola@gmail.com>
2021-04-23 06:22:17 +0300

    add input

--- InstrumentList.cpp
+++ InstrumentList.cpp
@@ -2,6 +2,7 @@
 
 #include "Controller.h"
 #include "Song.h"
+#include "UIInput.h"
 
 #include <fmt/core.h>
 
@@ -12,27 +13,65 @@ InstrumentList::render(bool refresh) {
   bool render_all = refresh;
   auto & song = getController().getSong();
 
+  auto [rows, cols] = getDim();
+
   if (song.getVersion() != current_song_version) {
     render_all = true;
   }
-  
+
+  bool need_refresh = false;
   if (render_all) {
     getPlane().drawBorder();
     
-    for (size_t i = 0; i < song.getInstruments().size(); i++) {
-      renderRow(i, *(song.getInstruments()[i]), false);
+    for (size_t i = 0; i < song.getInstruments().size() && i < rows - 2; i++) {
+      renderRow(i, i == current_cursor_row);
     }
-  } else {
-    
+    need_refresh = true;
+  } else if (new_cursor_row != current_cursor_row) {
+    renderRow(current_cursor_row, false);
+    renderRow(new_cursor_row, true);
+    need_refresh = true;
   }
 
   current_song_version = song.getVersion();
+  current_cursor_row = new_cursor_row;
   
-  return true;
+  return need_refresh;
 }
 
 void
-InstrumentList::renderRow(size_t row, const Instrument & instrument, bool highlight) {
-  auto & name = instrument.getName();
-  putstr(1 + row, 1, name);
+InstrumentList::renderRow(size_t row, bool highlight) {
+  auto & song = getController().getSong();
+  auto & instrument = *(song.getInstruments()[row]);
+  auto [rows, cols] = getDim();
+
+  if (highlight) {
+    setFgColor(0x00, 0x00, 0x00);
+    setBgColor(0xa0, 0xff, 0xa0);
+  } else {
+    setFgColor(0xe0, 0xe0, 0xe0);
+    setBgColor(0x20, 0x20, 0x20);
+  }
+  auto line = instrument.getName();
+  while (line.size() < cols - 2) line += ' ';
+  putstr(1 + row, 1, line);
+}
+
+bool
+InstrumentList::offerInput(const UIInput & input) {
+  auto & song = getController().getSong();
+  size_t num_rows = song.getInstruments().size();
+
+  if (input.getId() == NCKEY_UP) {
+    if (new_cursor_row > 0) new_cursor_row--;
+    return true;
+  } else if (input.getId() == NCKEY_DOWN) {
+    if (new_cursor_row + 1 < num_rows) new_cursor_row++;
+    return true;
+  } else if (input.getId() == NCKEY_ENTER) {
+    // select instrument
+    return true;
+  }
+  
+  return false;
 }
--- InstrumentList.h
+++ InstrumentList.h
@@ -3,21 +3,21 @@
 
 #include "UIElement.h"
 
-class Instrument;
-
 class InstrumentList : public UIElement {
  public:
   InstrumentList(UIPlane & parent) : UIElement(parent) {
 
   }
 
+  bool offerInput(const UIInput & input) override;
   bool render(bool refresh = false);
 
 protected:
-  void renderRow(size_t row, const Instrument & instrument, bool highlight);
+  void renderRow(size_t row, bool highlight);
 
  private:
-  int current_song_version = 0;  
+  int current_song_version = 0;
+  size_t new_cursor_row = 0, current_cursor_row = 0;
 };
 
 #endif
