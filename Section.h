f22b75481237be7f24b477a30c246ab532f139be
Mikael Rekola <mikael.rekola@gmail.com>
2021-04-24 15:49:27 +0300

    refactor, add non-const getTracks()

--- Section.h
+++ Section.h
@@ -3,12 +3,15 @@
 
 #include "Track.h"
 
+#define PATTLEN 32
+
 #include <vector>
 
 class Section {
  public:
   explicit Section() { }
 
+  std::vector<Track> & getTracks() { return tracks; }
   const std::vector<Track> & getTracks() const { return tracks; }
   const Track & getTrack(size_t i) const { return i < tracks.size() ? tracks[i] : empty_track; }
   Track & getTrack(size_t i) { return i < tracks.size() ? tracks[i] : empty_track; }
@@ -17,7 +20,7 @@ class Section {
   bool empty() const { return tracks.empty(); }
 
   size_t getTrackCount() const { return tracks.size(); }
-  size_t getRowCount() const { return 32; }
+  size_t getRowCount() const { return PATTLEN; }
 
 private:
   std::vector<Track> tracks;
