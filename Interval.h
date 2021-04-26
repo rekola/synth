fe046f6e3b241f1448388a531a8db34635fbdb6b
Mikael Rekola <mikael.rekola@gmail.com>
2021-04-26 04:41:48 +0300

    add class Interval

---
+++ Interval.h
@@ -0,0 +1,11 @@
+#ifndef _INTERVAL_H_
+#define _INTERVAL_H_
+
+class Interval {
+public:
+  Interval(short _numerator, short _denominator) : numerator(_numerator), denominator(_denominator) { }
+  
+private:
+};
+
+#endif
