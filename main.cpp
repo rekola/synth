d55240ee9f3c207a2d3d7cbae92b3819ae756722
Mikael Rekola <mikael.rekola@gmail.com>
2021-04-26 05:35:39 +0300

    fix whitespace, add more demos

--- main.cpp
+++ main.cpp
@@ -9,18 +9,30 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  bool load_demo = false;
+  int load_demo = 0;
+  bool relative = false;
+  
   for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "--demo") == 0) load_demo = true;
-    else {
+    if (strcmp(argv[i], "--demo") == 0) {
+      if (i + 1 < argc && argv[i + 1][0] != '-') {
+	i++;
+	load_demo = atoi(argv[i]);
+      } else {
+	load_demo = 1;
+      }
+    } else if (strcmp(argv[i], "--relative") == 0) {
+      relative = true;
+    } else {
       cerr << "invalid parameter\n";
       exit(1);
     }
   }
       
   auto controller = make_shared<Controller>();
-  if (load_demo) {
+  if (load_demo == 1) {
     controller->loadDemo();
+  } else if (load_demo == 2) {
+    controller->loadDemo2();
   } else {
     controller->createNewSong();
   }
