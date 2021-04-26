030eaa210987f1ea37a153ba049347912154786c
Mikael Rekola <mikael.rekola@gmail.com>
2021-04-26 22:10:39 +0300

    add playNote() method and multiple states

--- Track.h
+++ Track.h
@@ -2,6 +2,7 @@
 #define _TRACK_H_
 
 #include "Note.h"
+#include "Instrument.h"
 #include "InstrumentState.h"
 
 #include <deque>
@@ -24,20 +25,31 @@ class Track {
   void setInstrumentId(int id) { instrument_id = id; }
   int getInstrumentId() const { return instrument_id; }
 
-  InstrumentState & getState() { return state; }
+  std::vector<InstrumentState> & getStates() { return states; }
   
   void addPendingNotes(size_t frame, const std::vector<Note> & notes) {
     pending_notes.push_back(std::pair(frame, notes));
   }
   void clearPendingNotes() { pending_notes.clear(); }
   std::deque<std::pair<unsigned int, std::vector<Note> > > & getPendingNotes() { return pending_notes; }
-  
+
+  void playNote(const Note & note, const Instrument & instrument) {
+    for (auto & state : states) {
+      if (!state.isPlaying()) {
+	state.playNote(note, instrument.getTranspose(), instrument.getDetune());
+	return;
+      }
+    }
+    states.push_back(instrument.createVoice());
+    states.back().playNote(note, instrument.getTranspose(), instrument.getDetune());
+  }
+
 private:
   int instrument_id = 0;
-  InstrumentState state;
+  std::vector<InstrumentState> states;
   bool solo = false;
   float pan = 0.5f;
-  float volume = 0.5f;
+  float volume = 0.15f;
   std::shared_ptr<Track> first_child, next_sibling;
 
   std::deque<std::pair<unsigned int, std::vector<Note> > > pending_notes;
