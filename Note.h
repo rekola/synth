5531945c5f91b56d3c0586bc8bce12361549cdf1
Mikael Rekola <mikael.rekola@gmail.com>
2021-04-30 04:35:56 +0300

    refactor, add comments

--- Note.h
+++ Note.h
@@ -9,43 +9,45 @@
 
 class Note {
  public:
-  explicit Note() : note_number(0), velocity(0x3f) { }
-  explicit Note(int _note_number, short _velocity = 0x3f) : note_number(_note_number), velocity(_velocity) { }
-  explicit Note(std::string value, short _velocity = 0x3f, Tuning tuning = Tuning::TET12) : velocity(_velocity) {
+  explicit Note() : value(-1), velocity(0x3f) { }
+  explicit Note(int _value, short _velocity = 0x3f) : value(_value), velocity(_velocity) { }
+  explicit Note(std::string input_value, short _velocity = 0x3f, Tuning tuning = Tuning::TET12) : velocity(_velocity) {
     if (tuning == Tuning::TET12) {
-      char letter = value[0];
-      char accidental = value[1];
+      char letter = input_value[0];
+      char accidental = input_value[1];
 
-      int octave = stoi(value.substr(2));
-      note_number = (octave + 1) * 12;
+      int octave = stoi(input_value.substr(2));
+      value = (octave + 1) * 12;
       
       // C C# D D# E F F# G G# A A# B
 
       assert(letter >= 'A' && letter <= 'G');
-      if (letter >= 'C' && letter <= 'E') note_number += (letter - 'C') * 2;
-      else if (letter == 'F' || letter == 'G') note_number += 5 + (letter - 'F') * 2;
-      else if (letter == 'A' || letter == 'B') note_number += 9 + (letter - 'A') * 2;
+      if (letter >= 'C' && letter <= 'E') value += (letter - 'C') * 2;
+      else if (letter == 'F' || letter == 'G') value += 5 + (letter - 'F') * 2;
+      else if (letter == 'A' || letter == 'B') value += 9 + (letter - 'A') * 2;
 
       assert(accidental == '#' || accidental == 'b' || accidental == '-');
-      if (accidental == '#') note_number++;
-      else if (accidental == 'b') note_number--;
+      if (accidental == '#') value++;
+      else if (accidental == 'b') value--;
     } else {
       assert(0);
-      note_number = 0;
+      value = 0;
     }
   }
 
-  short getNoteNumber() const { return note_number; }
+  short getValue() const { return value; }
   short getVelocity() const { return velocity; }
   float getVelocityAsFloat() const { return (float)velocity / 0x3f; }
-  bool isDefined() const { return note_number > 0; }
-  bool isOff() const { return note_number == 1 || velocity == 0; }
+  bool isDefined() const { return value >= 0; }
+  bool isOff() const { return value == 0 || velocity == 0; }
   
   inline float getFrequency(Tuning tuning, int transpose, int detune) {
-    if (tuning == Tuning::TET31) {
-      return 440.0f * powf(2.0f, (note_number - 178.0f + transpose + detune / 100.0f) / 31.0f);
+    if (isOff()) {
+      return 0.0f;
+    } else if (tuning == Tuning::TET31) {
+      return 440.0f * powf(2.0f, (value - 178.0f + transpose + detune / 100.0f) / 31.0f);
     } else if (tuning == Tuning::TET12) {
-      return 440.0f * powf(2.0f, (note_number - 69.0f + transpose + detune / 100.0f) / 12.0f);
+      return 440.0f * powf(2.0f, (value - 69.0f + transpose + detune / 100.0f) / 12.0f);
     } else {
       assert(0);
     }
@@ -53,7 +55,7 @@ class Note {
 
   std::string toString(Tuning tuning) const {
     if (isOff()) return "OFF";
-    else if (note_number > 1) {
+    else if (isDefined()) {
       static const char * note_names_31tet[] = { "C-", "D𝄫", "C♯", "D♭", "C𝄪", "D-", "E𝄫", "D♯",
 						 "E♭", "D𝄪", "E-", "F♭", "E♯", "F-", "G𝄫", "F♯",
 						 "G♭", "F𝄪", "G-", "A𝄫", "G♯", "A♭", "G𝄪", "A-",
@@ -61,17 +63,18 @@ class Note {
       static const char * note_names_12tet[] = { "C-", "C♯", "D-", "D♯", "E-", "F-", "F♯", "G-", "G♯", "A-", "A♯", "B-" };
       
       if (tuning == Tuning::TET31) {
-	return std::string(note_names_31tet[note_number % 31]) + std::to_string((note_number / 31) - 1);
+	return std::string(note_names_31tet[value % 31]) + std::to_string((value / 31) - 1);
       } else {
-	return std::string(note_names_12tet[note_number % 12]) + std::to_string((note_number / 12) - 1);
+	return std::string(note_names_12tet[value % 12]) + std::to_string((value / 12) - 1);
       }
     } else {
-      return "???";
+      return "n/a";
     }
   }
   
  private:
-  short note_number, velocity;
+  int value; // sample position, note value or -1 for undefined note
+  short velocity;
   // note specific tuning
 };
 
