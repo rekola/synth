d1ef5b7c8b5e4ae31f9f039a52975a43e8011eca
Mikael Rekola <mikael.rekola@gmail.com>
2021-05-03 21:56:47 +0300

    implement from text conversion for TET-31 notes, add getPanning()

--- Note.h
+++ Note.h
@@ -16,31 +16,65 @@ class Note {
     replace(input_value, "♭", "b");
     replace(input_value, "𝄫", "bb");
     replace(input_value, "𝄪", "x");
-    
-    if (tuning == Tuning::TET12) {
-      char letter = input_value[0];
-      char accidental = input_value[1];
 
-      int octave = stoi(input_value.substr(2));
+    int octave;
+    std::string::size_type pos = input_value.find_first_of("0123456789");
+    if (pos != std::string::npos) {
+      octave = stoi(input_value.substr(pos));
+      input_value.erase(pos);
+    } else {
+      octave = 4;
+    }
+    char letter = input_value[0];
+    std::string accidental = input_value.substr(1);
+
+    assert(letter >= 'A' && letter <= 'G');
+      
+    if (tuning == Tuning::TET12) {
       value = (octave + 1) * 12;
       
       // C C# D D# E F F# G G# A A# B
 
-      assert(letter >= 'A' && letter <= 'G');
       if (letter >= 'C' && letter <= 'E') value += (letter - 'C') * 2;
       else if (letter == 'F' || letter == 'G') value += 5 + (letter - 'F') * 2;
       else if (letter == 'A' || letter == 'B') value += 9 + (letter - 'A') * 2;
-
-      assert(accidental == '#' || accidental == 'b' || accidental == '-');
-      if (accidental == '#') value++;
-      else if (accidental == 'b') value--;
+      else {
+	assert(0);
+      }
+      
+      if (accidental == "#") value++;
+      else if (accidental == "b") value--;
+      else {
+	assert(accidental == "-");
+      }
     } else if (tuning == Tuning::TET19) {
+      value = (octave + 1) * 19;
+
       // C C♯ D♭ D D♯ E♭ E E♯ F♭ F F♯ G♭ G G♯ A♭ A A♯ B♭ B B♯ C♭
       assert(0);
       value = 0;
     } else {
-      assert(0);
-      value = 0;
+      value = (octave + 1) * 31;
+      
+      // C D𝄫 C♯ D♭ C𝄪 D E𝄫 D♯,
+      // E♭ D𝄪 E F♭ E♯ F G𝄫 F♯,
+      // G♭ F𝄪 G A𝄫 G♯ A♭ G𝄪 A,
+      // B𝄫 A♯ B♭ A𝄪 B C♭ B♯
+
+      if (letter >= 'C' && letter <= 'E') value += (letter - 'C') * 5;
+      else if (letter >= 'F' && letter <= 'G') value += 13 + (letter - 'F') * 5;
+      else if (letter >= 'A' && letter <= 'B') value += 23 + (letter - 'A') * 5;
+      else {
+	assert(0);
+      }
+
+      if (accidental == "#") value += 2;
+      else if (accidental == "b") value -= 2;
+      else if (accidental == "x") value += 4;
+      else if (accidental == "bb") value -= 4;
+      else {
+	assert(accidental == "-");
+      }
     }
   }
 
@@ -92,6 +126,25 @@ class Note {
       }
     } else {
       return "n/a";
+    }
+  }
+
+  float getPanning(Tuning tuning) const {
+    if (isOff()) {
+      return 0.5f;
+    } else {
+      float p;
+      if (tuning == Tuning::TET12) {
+	p = 0.5f + 0.5f * (value - 69.0f) / 12.0f;
+      } else if (tuning == Tuning::TET31) {
+	p = 0.5f + 0.5f * (value - 178.0f) / 31.0f;
+      } else {
+	return 0.5f;
+      }
+
+      if (p < 0.0f) p = 0.0f;
+      else if (p > 1.0f) p = 1.0f;
+      return p;
     }
   }
   
