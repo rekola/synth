eb8cf98977972d677c268db34ecc182a93fe303d
Mikael Rekola <mikael.rekola@gmail.com>
2021-05-04 16:19:35 +0300

    add Mixer

---
+++ Mixer.h
@@ -0,0 +1,19 @@
+#ifndef _MIXER_H_
+#define _MIXER_H_
+
+#include <cstddef>
+
+class Mixer {
+ public:
+  Mixer() { }
+  virtual ~Mixer() { }
+
+  virtual void reset() = 0;
+  virtual void accumulate(const float * input, size_t frames, float distance = 0.0f, float azimuth = 0.0f, float elevation = 0.0f) = 0;
+  virtual void encode(float * output, size_t frames, float master_volume) = 0;
+  
+ private:
+  
+};
+
+#endif
