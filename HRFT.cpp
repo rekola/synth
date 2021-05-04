63b169579bf2783afdaedea29946a7efe70c3389
Mikael Rekola <mikael.rekola@gmail.com>
2021-05-04 17:34:02 +0300

    refactor and optimize by allowing accumulation

--- HRFT.cpp
+++ HRFT.cpp
@@ -10,63 +10,83 @@
 using namespace std;
 
 void
-HRFT::filterAndMix(const float * input, float * output, size_t frames) {
-  if (!is_initialized) {
-    is_initialized = true;
+HRFT::initialize(size_t frames) {
+  is_initialized = true;
+      
+  int order = 1;
+  bool is_3d = true;
     
-    PolarPoint position;
-    position.fElevation = elevation * M_PI / 180.0f;
-    position.fAzimuth = azimuth * M_PI / 180.0f;
-    position.fDistance = 5;
-
-    int order = 1;
-    bool is_3d = true;
-    
-    myEncoder = make_shared<CAmbisonicEncoder>();
-    // myEncoder->SetRoomRadius(10);
-    if (!myEncoder->Configure(order, is_3d, 0)) {
-      cerr << "encoder configure failed\n";
-      exit(1);
-    }
-    myEncoder->SetPosition(position);
-    myEncoder->Refresh();
-
-    myBinauralizer = make_shared<CAmbisonicBinauralizer>();
-    unsigned tailLength;
-    if (!myBinauralizer->Configure(order, is_3d, 44100, frames, tailLength, "")) { //./SADIE_KEMAR_DFC_256_order_fir_48000")) {
-      // /home/rekola/src/personal/syna/D1_44K_24bit_256tap_FIR_SOFA.sofa")) {
-      cerr << "decoder config failed\n";
-      exit(1);
-    }
-    myBinauralizer->Refresh();
-    
-    buffers[0] = new float[frames];
-    buffers[1] = new float[frames];
-
-    myBFormat = make_shared<CBFormat>();
-    if (!myBFormat->Configure(order, is_3d, frames)) {
-      cerr << "format config failed\n";
-      exit(1);
-    }
+  myEncoder = make_shared<CAmbisonicEncoder>();
+  // myEncoder->SetRoomRadius(10);
+  if (!myEncoder->Configure(order, is_3d, 0)) {
+    cerr << "encoder configure failed\n";
+    exit(1);
   }
+  
+  myBinauralizer = make_shared<CAmbisonicBinauralizer>();
+  unsigned tailLength;
+  if (!myBinauralizer->Configure(order, is_3d, 44100, frames, tailLength, "")) { //./SADIE_KEMAR_DFC_256_order_fir_48000")) {
+    // /home/rekola/src/personal/syna/D1_44K_24bit_256tap_FIR_SOFA.sofa")) {
+    cerr << "decoder config failed\n";
+    exit(1);
+  }
+  myBinauralizer->Refresh();
+  
+  buffers[0] = new float[frames];
+  buffers[1] = new float[frames];
+  
+  myBFormat = make_shared<CBFormat>();
+  if (!myBFormat->Configure(order, is_3d, frames)) {
+    cerr << "format config failed\n";
+    exit(1);
+  }
+}
+
+void
+HRFT::reset() {
+  if (is_initialized) myBFormat->Reset();
+}
+
+void
+HRFT::accumulate(const float * input, size_t frames, float distance, float azimuth, float elevation) {
+  if (!is_initialized) initialize(frames);
+
+  PolarPoint position;
+  position.fDistance = distance;
+  position.fAzimuth = azimuth * M_PI / 180.0f;
+  position.fElevation = elevation * M_PI / 180.0f;
+
+  myEncoder->SetPosition(position);
+  myEncoder->Refresh();
 
   assert(frames == 1024);
   
-  assert(myEncoder.get());
-  assert(myBinauralizer.get());
+  // memset(left, 0, frames * sizeof(float));
+  // memset(right, 0, frames * sizeof(float));
+  
+  myEncoder->ProcessAccumul((float *)input, frames, myBFormat.get());
+}
+
+void
+HRFT::encode(float * output, size_t frames, float master_volume) {
+  if (!is_initialized) initialize(frames);
+
+  myBinauralizer->Process(myBFormat.get(), buffers);
 
   float * left = buffers[0];
   float * right = buffers[1];
 
-  // memset(left, 0, frames * sizeof(float));
-  // memset(right, 0, frames * sizeof(float));
-  // myBFormat->Reset();
-  
-  myEncoder->Process((float *)input, frames, myBFormat.get());
-  myBinauralizer->Process(myBFormat.get(), buffers);
-
   for (size_t i = 0; i < frames; i++) {
-    output[2 * i + 0] += left[i];
-    output[2 * i + 1] += right[i];
+    float l = left[i] * master_volume;
+    float r = right[i] * master_volume;
+
+    if (l > 1.0) l = 1.0;
+    else if (l < -1.0) l = -1.0;
+
+    if (r > 1.0) r = 1.0;
+    else if (r < -1.0) r = -1.0;
+
+    output[2 * i + 0] = l;
+    output[2 * i + 1] = r;
   }
 }
--- HRFT.h
+++ HRFT.h
@@ -1,25 +1,25 @@
 #ifndef _HRFT_H_
 #define _HRFT_H_
 
+#include "Mixer.h"
+
 #include <memory>
 
 class CAmbisonicBinauralizer;
 class CAmbisonicEncoder;
 class CBFormat;
 
-class HRFT {
+class HRFT : public Mixer {
  public:
   HRFT() { }
   
-  void filterAndMix(const float * input, float * output, size_t frames);
-
-  void setElevation(float e) { elevation = e; }
-  void setAzimuth(float a) { azimuth = a; }
-  void setDistance(float d) { distance = d; }
+  void reset() override;
+  void accumulate(const float * input, size_t frames, float distance, float azimuth, float elevation) override;
+  void encode(float * output, size_t frames, float master_volume) override;
   
 private:
-  float elevation = 0, azimuth = 0, distance = 0;
-
+  void initialize(size_t frames);
+  
   bool is_initialized = false;
   std::shared_ptr<CAmbisonicEncoder> myEncoder;
   std::shared_ptr<CAmbisonicBinauralizer> myBinauralizer;
