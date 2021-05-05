786f21e07c4a30e44fa32bf9c99a6b362e956162
Mikael Rekola <mikael.rekola@gmail.com>
2021-05-05 07:44:25 +0300

    reset all fields

--- Envelope.h
+++ Envelope.h
@@ -5,7 +5,7 @@ class Envelope {
  public:
   Envelope() : delay(0.0f), attack(0.0f), hold(0.0f), decay(0.0f), sustain(1.0f), release(0.0f), keynumToHold(0.0f), keynumToDecay(0.0f) { }
   Envelope(float _a, float _d, float _s, float _r)
-    : attack(_a), decay(_d), sustain(_s), release(_r)
+    : delay(0.0f), attack(_a), hold(0.0f), decay(_d), sustain(_s), release(_r), keynumToHold(0.0f), keynumToDecay(0.0f)
     {
       
     }
