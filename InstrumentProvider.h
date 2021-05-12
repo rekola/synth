d3470b2589a2ef92cb4c20d03e0ed2436e61ce55
Mikael Rekola <mikael.rekola@gmail.com>
2021-05-12 03:11:54 +0300

    refactor, add default instrument

--- InstrumentProvider.h
+++ InstrumentProvider.h
@@ -1,8 +1,11 @@
+
 #ifndef _INSTRUMENTPROVIDER_H_
 #define _INSTRUMENTPROVIDER_H_
 
 #include "Instrument.h"
 #include "SoundFont.h"
+#include "SubtractiveInstrument.h"
+#include "Filter.h"
 
 #include <string>
 #include <memory>
@@ -10,15 +13,22 @@
 
 class InstrumentProvider {
  public:
-  InstrumentProvider() { }
+  InstrumentProvider() {
+    auto epiano = std::make_shared<SubtractiveInstrument>(WaveformType::SAW);
+    epiano->setName("Electric Piano");
+    epiano->setAmpEnvelope(Envelope(0.0f, 10 * 20 / 255.0f, 0.0f, 0.0));
+    epiano->addEffect(std::make_unique<Filter>(63 / 255.0f, 128 / 63.0f, false));
+    addInstrument(epiano);
+
+    default_instrument = epiano;
+  }
 
   void loadSoundFont(std::string filename) {
     auto sf = std::make_unique<SoundFont>(filename);
     auto instruments = sf->createAll();
     for (auto & instrument : instruments) {
-      auto & name = instrument->getName();
-      if (!name.empty()) {
-	instruments_by_name[name] = std::move(instrument);
+      if (!instrument->getName().empty()) {
+	addInstrument(move(instrument));
       }
     }
   }
@@ -28,14 +38,21 @@ class InstrumentProvider {
     if (it != instruments_by_name.end()) {
       return it->second;
     } else {
-      return std::shared_ptr<Instrument>(0);
+      return default_instrument;
     }
   }
 
-  
+protected:
+  void addInstrument(std::unique_ptr<Instrument> instrument) {
+    addInstrument(move(instrument));
+  }
+  void addInstrument(std::shared_ptr<Instrument> instrument) {
+    instruments_by_name[instrument->getName()] = instrument;
+  }
   
  private:
   std::unordered_map<std::string, std::shared_ptr<Instrument> > instruments_by_name;
+  std::shared_ptr<Instrument> default_instrument;
 };
 
 #endif
