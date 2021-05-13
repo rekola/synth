3dda12410f57881752700ace626eeb4305982462
Mikael Rekola <mikael.rekola@gmail.com>
2021-05-13 23:32:50 +0300

    add percussion

--- InstrumentProvider.h
+++ InstrumentProvider.h
@@ -31,6 +31,63 @@ class InstrumentProvider {
 	addInstrument(move(instrument));
       }
     }
+
+// 27 High Q
+// 28 Slap
+// 29 Stratch Push
+// 30 Stratch Pull
+// 31 Sticks
+// 32 Square Click
+// 33 Metronome Click
+// 34 Metronome Bell
+    addInstrument(sf->createInstrument(160, 35, "Acoustic Bass Drum"));
+    addInstrument(sf->createInstrument(160, 36, "Electric Bass Drum"));
+// 37 Side Stick
+    addInstrument(sf->createInstrument(160, 38, "Acoustic Snare"));
+// 39 Hand Clap
+    addInstrument(sf->createInstrument(160, 40, "Electric Snare"));
+// 41 Low Floor Tom
+    addInstrument(sf->createInstrument(160, 42, "Closed Hi-hat"));
+// 43 High Floor Tom
+// 44 Pedal Hi-hat
+// 45 Low Tom
+    addInstrument(sf->createInstrument(160, 46, "Open Hi-hat"));
+// 47 Low-Mid Tom
+// 48 Hi-Mid Tom
+// 49 Crash Cymbal 1
+// 50 High Tom
+// 51 Ride Cymbal 1
+// 52 Chinese Cymbal
+// 53 Ride Bell
+    addInstrument(sf->createInstrument(160, 54, "Tambourine"));
+// 55 Splash Cymbal
+// 56 Cowbell
+// 57 Crash Cymbal 2
+// 58 Vibra Slap
+// 59 Ride Cymbal 2
+// 60 High Bongo
+// 61 Low Bongo
+// 62 Mute High Conga
+// 63 Open High Conga
+// 64 Low Conga
+// 65 High Timbale
+// 66 Low Timbale
+// 67 High Agogô
+// 68 Low Agogô
+// 69 Cabasa
+    addInstrument(sf->createInstrument(160, 70, "Maracas"));
+// 71 Short Whistle
+// 72 Long Whistle
+// 73 Short Guiro
+// 74 Long Guiro
+// 75 Claves
+// 76 High Woodblock
+// 77 Low Woodblock
+// 78 Mute Cuica
+// 79 Open Cuica
+// 80 Mute Triangle
+// 81 Open Triangle
+    addInstrument(sf->createInstrument(160, 82, "Shaker"));
   }
   
   std::shared_ptr<Instrument> getInstrumentByName(std::string name) const {
