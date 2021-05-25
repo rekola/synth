6a34a88e362b2bd6b6a42f6a2690b5222f99f7f6
Mikael Rekola <mikael.rekola@gmail.com>
2021-05-26 01:04:28 +0300

    add TANH, add XML

--- Distortion.cpp
+++ Distortion.cpp
@@ -2,6 +2,8 @@
 
 #include "SampleData.h"
 
+#include "tinyxml2.h"
+
 using namespace std;
 
 class DistortionState : public EffectState {
@@ -21,6 +23,24 @@ public:
 	buffer[i] = drymix * x + (1.0f - drymix) * y;
       }
       break;
+
+    case DistortionType::TANH:
+      {
+	float timbre = 1.0f;
+	float depth = 1.0f;
+	float timbreInverse = (1 - (timbre * 0.099)) * 10;
+	for (size_t i = 0; i < input.getChannels() * input.size(); i++) {
+	  float x = buffer[i];
+	  x *= depth;
+	  x = tanhf(x * (timbre + 1));
+	  x = x * ((0.1 + timbre) * timbreInverse);
+	  x = cos((x + (timbre + 0.25)));
+	  x = tanh(x * (timbre + 1));                  
+	  x = x * 0.125;                      
+	  buffer[i] = x;
+      	}
+      }
+      break;
        
     case DistortionType::BITCRUSH:
       break;
@@ -37,3 +57,21 @@ Distortion::createState(unsigned int outSampleRate) const {
   return make_unique<DistortionState>(outSampleRate, type, param, drymix);
 }
 
+void
+Distortion::readXML(tinyxml2::XMLElement & element) {
+  Effect::readXML(element);
+  
+  auto param_text = element.Attribute("param");
+  param = param_text ? atof(param_text) : 1.0f;
+
+  auto type_text = element.Attribute("type");
+  if (type_text) {
+    if (strcmp(type_text, "clip") == 0) type = DistortionType::CLIP;
+    else if (strcmp(type_text, "tanh") == 0) type = DistortionType::TANH;
+  }
+}
+
+void
+Distortion::populateXML(tinyxml2::XMLElement & element) const {
+  Effect::populateXML(element);  
+}
--- Distortion.h
+++ Distortion.h
@@ -3,13 +3,16 @@
 
 #include "Effect.h"
 
-enum class DistortionType { CLIP = 1, BITCRUSH };
+enum class DistortionType { CLIP = 1, TANH, BITCRUSH };
 
 class Distortion : public Effect {
  public:
+  Distortion() : type(DistortionType::CLIP), param(1.0f), drymix(0) { }
   Distortion(DistortionType _type, float _param, float _drymix) : type(_type), param(_param), drymix(_drymix) { }
 
   std::unique_ptr<EffectState> createState(unsigned int outSamplerate) const override;
+  void readXML(tinyxml2::XMLElement & element) override;
+  void populateXML(tinyxml2::XMLElement & element) const override;
 
  private:
   DistortionType type;
