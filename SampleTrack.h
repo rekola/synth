085730d5e39e6b5cea66d551ff359c18b1c712e2
Mikael Rekola <mikael.rekola@gmail.com>
2021-05-28 19:49:28 +0300

    return empty track

--- SampleTrack.h
+++ SampleTrack.h
@@ -8,7 +8,7 @@ public:
   SampleTrack(const std::shared_ptr<SampleData> & _sample) : Track(SAMPLE), sample(_sample) { }
 
   SampleData render(size_t frames, SongState & song_state, const std::vector<std::unique_ptr<Instrument> > & instruments, TrackEventQueue & events) override {
-    return SampleData();
+    return SampleData(1, frames);
   }
 
   void setSample(std::shared_ptr<SampleData> _sample) { sample = _sample; }
