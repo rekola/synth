841836193278ded2da1ee27b4e3e9fe693f96737
Mikael Rekola <mikael.rekola@gmail.com>
2021-05-28 17:07:45 +0300

    refactor

--- GroupTrack.h
+++ GroupTrack.h
@@ -7,19 +7,7 @@ class GroupTrack : public Track {
  public:
   GroupTrack() : Track(GROUP) { }
 
-  SampleData render(size_t frames, SongState & song_state, const std::vector<std::unique_ptr<Instrument> > & instruments, TrackEventQueue & events) override {
-    if (getChildren().empty()) {
-      return SampleData(1, frames);
-    } else {
-      auto it = getChildren().begin();
-      auto sd = (*it)->render(frames, song_state, instruments, events);
-      for ( ; it != getChildren().end(); it++) {
-	auto sd2 = (*it)->render(frames, song_state, instruments, events);
-	sd.mix(sd2);
-      }
-      return sd;
-    }
-  }  
+  virtual std::string getElementName() const override { return "group"; }
 };
 
 #endif
