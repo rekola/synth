c90e057e5ddd320ff7890fc962bef33e6e75ebf7
Mikael Rekola <mikael.rekola@gmail.com>
2022-01-14 13:47:02 +0200

    update instruments

--- InstrumentProvider.h
+++ InstrumentProvider.h
@@ -22,71 +22,90 @@ class InstrumentProvider {
     default_instrument = epiano;
   }
 
-  void loadSoundFont(std::string filename) {
+  void loadSoundFont(std::string filename, bool as_midi_default = true) {
     auto sf = std::make_unique<SoundFont>(filename);
-    auto instruments = sf->createAll();
-    for (auto & instrument : instruments) {
-      if (!instrument->getName().empty()) {
-	addInstrument(move(instrument));
+
+    if (as_midi_default) {
+      // Piano
+      addInstrument(sf->createInstrument(0, 0, "Acoustic Grand Piano"));
+      addInstrument(sf->createInstrument(1, 0, "Bright Acoustic Piano"));
+      addInstrument(sf->createInstrument(2, 0, "Electric Grand Piano"));
+      addInstrument(sf->createInstrument(3, 0, "Honky-tonk Piano"));
+      addInstrument(sf->createInstrument(4, 0, "Rhodes Piano"));
+      addInstrument(sf->createInstrument(5, 0, "Chorused Piano"));
+      addInstrument(sf->createInstrument(6, 0, "Harpsichord"));
+      addInstrument(sf->createInstrument(7, 0, "Clavinet"));
+      
+      addInstrument(sf->createInstrument(34, 0, "Electric Bass (finger)"));
+      addInstrument(sf->createInstrument(42, 0, "Cello"));
+      addInstrument(sf->createInstrument(45, 0, "Pizzicato Strings"));
+
+      addInstrument(sf->createInstrument(88, 0, "Pad 1 (new age)"));
+		    
+      // 27 High Q
+      // 28 Slap
+      // 29 Stratch Push
+      // 30 Stratch Pull
+      // 31 Sticks
+      // 32 Square Click
+      // 33 Metronome Click
+      // 34 Metronome Bell
+      addInstrument(sf->createInstrument(160, 35, "Acoustic Bass Drum"));
+      addInstrument(sf->createInstrument(160, 36, "Electric Bass Drum"));
+      // 37 Side Stick
+      addInstrument(sf->createInstrument(160, 38, "Acoustic Snare"));
+      // 39 Hand Clap
+      addInstrument(sf->createInstrument(160, 40, "Electric Snare"));
+      // 41 Low Floor Tom
+      addInstrument(sf->createInstrument(160, 42, "Closed Hi-hat"));
+      // 43 High Floor Tom
+      // 44 Pedal Hi-hat
+      // 45 Low Tom
+      addInstrument(sf->createInstrument(160, 46, "Open Hi-hat"));
+      // 47 Low-Mid Tom
+      // 48 Hi-Mid Tom
+      // 49 Crash Cymbal 1
+      // 50 High Tom
+      // 51 Ride Cymbal 1
+      // 52 Chinese Cymbal
+      // 53 Ride Bell
+      addInstrument(sf->createInstrument(160, 54, "Tambourine"));
+      // 55 Splash Cymbal
+      // 56 Cowbell
+      // 57 Crash Cymbal 2
+      // 58 Vibra Slap
+      // 59 Ride Cymbal 2
+      // 60 High Bongo
+      addInstrument(sf->createInstrument(160, 61, "Low Bongo"));
+      // 62 Mute High Conga
+      // 63 Open High Conga
+      // 64 Low Conga
+      // 65 High Timbale
+      // 66 Low Timbale
+      // 67 High Agogô
+      // 68 Low Agogô
+      // 69 Cabasa
+      addInstrument(sf->createInstrument(160, 70, "Maracas"));
+      // 71 Short Whistle
+      // 72 Long Whistle
+      // 73 Short Guiro
+      // 74 Long Guiro
+      // 75 Claves
+      // 76 High Woodblock
+      // 77 Low Woodblock
+      // 78 Mute Cuica
+      // 79 Open Cuica
+      // 80 Mute Triangle
+      // 81 Open Triangle
+      addInstrument(sf->createInstrument(160, 82, "Shaker"));
+    } else {
+      auto instruments = sf->createAll();
+      for (auto & instrument : instruments) {
+	if (!instrument->getName().empty()) {
+	  addInstrument(move(instrument));
+	}
       }
     }
-
-// 27 High Q
-// 28 Slap
-// 29 Stratch Push
-// 30 Stratch Pull
-// 31 Sticks
-// 32 Square Click
-// 33 Metronome Click
-// 34 Metronome Bell
-    addInstrument(sf->createInstrument(160, 35, "Acoustic Bass Drum"));
-    addInstrument(sf->createInstrument(160, 36, "Electric Bass Drum"));
-// 37 Side Stick
-    addInstrument(sf->createInstrument(160, 38, "Acoustic Snare"));
-// 39 Hand Clap
-    addInstrument(sf->createInstrument(160, 40, "Electric Snare"));
-// 41 Low Floor Tom
-    addInstrument(sf->createInstrument(160, 42, "Closed Hi-hat"));
-// 43 High Floor Tom
-// 44 Pedal Hi-hat
-// 45 Low Tom
-    addInstrument(sf->createInstrument(160, 46, "Open Hi-hat"));
-// 47 Low-Mid Tom
-// 48 Hi-Mid Tom
-// 49 Crash Cymbal 1
-// 50 High Tom
-// 51 Ride Cymbal 1
-// 52 Chinese Cymbal
-// 53 Ride Bell
-    addInstrument(sf->createInstrument(160, 54, "Tambourine"));
-// 55 Splash Cymbal
-// 56 Cowbell
-// 57 Crash Cymbal 2
-// 58 Vibra Slap
-// 59 Ride Cymbal 2
-// 60 High Bongo
-    addInstrument(sf->createInstrument(160, 61, "Low Bongo"));
-// 62 Mute High Conga
-// 63 Open High Conga
-// 64 Low Conga
-// 65 High Timbale
-// 66 Low Timbale
-// 67 High Agogô
-// 68 Low Agogô
-// 69 Cabasa
-    addInstrument(sf->createInstrument(160, 70, "Maracas"));
-// 71 Short Whistle
-// 72 Long Whistle
-// 73 Short Guiro
-// 74 Long Guiro
-// 75 Claves
-// 76 High Woodblock
-// 77 Low Woodblock
-// 78 Mute Cuica
-// 79 Open Cuica
-// 80 Mute Triangle
-// 81 Open Triangle
-    addInstrument(sf->createInstrument(160, 82, "Shaker"));
   }
   
   std::shared_ptr<Instrument> getInstrumentByName(std::string name) const {
