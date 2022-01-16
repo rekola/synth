bc31336ce91e022825ef9acd07e024ca9a293550
Mikael Rekola <mikael.rekola@gmail.com>
2022-01-16 08:30:45 +0200

    remove file

--- Instrument.cpp
+++
@@ -1,8 +0,0 @@
-#include "Instrument.h"
-#include "VoicePool.h"
-
-void
-Instrument::playNote(size_t column, float frequency, float velocity, float delay, float detune, VoicePool & voices) const {
-  voices.stopVoices(column);
-  voices.getVoice(column, *this).playNote(frequency, velocity, delay, detune);
-}
