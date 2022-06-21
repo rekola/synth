1d7934e37353944046c5aa179a1d2a449a9a0f4d
Mikael Rekola <mikael.rekola@gmail.com>
2022-06-21 22:40:15 +0300

    refactor

--- NoteMultiplier.cpp
+++ NoteMultiplier.cpp
@@ -5,20 +5,19 @@
 using namespace std;
 
 std::unique_ptr<TrackState>
-NoteMultiplier::playNote(ChannelConfiguration channel_config, int outSampleRate, float azimuth, float frequency, float velocity, float start_phase) const {
-  auto group = createState(channel_config, outSampleRate);
+NoteMultiplier::playNote(const ChannelConfiguration & channel_config, float azimuth, float frequency, float velocity, float start_phase) const {
+  auto group = createState(channel_config);
   for (auto & child : getChildren()) {
     float unison_velocity = velocity; // / (unisons + 1);
     
     // root
-    auto voice = child->playNote(channel_config, outSampleRate, azimuth, frequency, unison_velocity, start_phase + (float)rand() / RAND_MAX);
+    auto voice = child->playNote(channel_config, azimuth, frequency, unison_velocity, start_phase + (float)rand() / RAND_MAX);
     if (voice.get()) group->addChild(move(voice));
 
     float frequency_start = frequency * (1 - detune / 2);
     float frequency_end = frequency * (1 + detune / 2);
 
     float azimuth_start = azimuth - spread / 2;
-    float azimuth_end = azimuth + spread / 2;
 
     if (unisons >= 2) {
       float frequency_step = powf(frequency_end / frequency_start, 1.0f / (unisons - 1));
@@ -26,18 +25,18 @@ NoteMultiplier::playNote(ChannelConfiguration channel_config, int outSampleRate,
       
       // unisons
       for (int i = 0; i < unisons; i++) {
-	auto voice = child->playNote(channel_config, outSampleRate, azimuth_start + i * azimuth_step, frequency_start * powf(frequency_step, i), unison_velocity, start_phase + (float)rand() / RAND_MAX);
+	auto voice = child->playNote(channel_config, azimuth_start + i * azimuth_step, frequency_start * powf(frequency_step, i), unison_velocity, start_phase + (float)rand() / RAND_MAX);
 	if (voice.get()) group->addChild(move(voice));
       }
     }
 
-    size_t i;
+    int i;
     float m;
     // octaves
     for (i = 0, m = 2; i < octaves; i++, m *= 2) {
       float f = frequency;
       if (detune != 0) f *= (float)rand() / RAND_MAX * (detune - 0.5 * detune);
-      auto voice = child->playNote(channel_config, outSampleRate, azimuth, f * m, velocity / m, start_phase + (float)rand() / RAND_MAX);
+      auto voice = child->playNote(channel_config, azimuth, f * m, velocity / m, start_phase + (float)rand() / RAND_MAX);
       if (voice.get()) group->addChild(move(voice));
     }
 
@@ -45,14 +44,14 @@ NoteMultiplier::playNote(ChannelConfiguration channel_config, int outSampleRate,
     for (i = 0, m = 3.0f / 2.0f; i < fifths; i++, m *= 3.0f / 2.0f) {
       float f = frequency;
       if (detune != 0) f *= (float)rand() / RAND_MAX * (detune - 0.5 * detune);
-      auto voice = child->playNote(channel_config, outSampleRate, azimuth, f * m, velocity / m, start_phase + (float)rand() / RAND_MAX);
+      auto voice = child->playNote(channel_config, azimuth, f * m, velocity / m, start_phase + (float)rand() / RAND_MAX);
       if (voice.get()) group->addChild(move(voice));
     }
 
     for (i = 0, m = 4.0f / 3.0f; i < fourths; i++, m *= 4.0f / 3.0f) {
       float f = frequency;
       if (detune != 0) f *= (float)rand() / RAND_MAX * (detune - 0.5 * detune);
-      auto voice = child->playNote(channel_config, outSampleRate, azimuth, f * m, velocity / m, start_phase + (float)rand() / RAND_MAX);
+      auto voice = child->playNote(channel_config, azimuth, f * m, velocity / m, start_phase + (float)rand() / RAND_MAX);
       if (voice.get()) group->addChild(move(voice));
     }
 }
@@ -61,7 +60,7 @@ NoteMultiplier::playNote(ChannelConfiguration channel_config, int outSampleRate,
 
 void
 NoteMultiplier::loadParameters(const ParameterSource & input) {
-  Effect::loadParameters(input);
+  Track::loadParameters(input);
 
   unisons = input.getInt("unisons");
   octaves = input.getInt("octaves");
@@ -78,5 +77,5 @@ NoteMultiplier::loadParameters(const ParameterSource & input) {
 
 void
 NoteMultiplier::storeParameters(ParameterSource & output) const {
-  Effect::storeParameters(output);
+  Track::storeParameters(output);
 }
--- NoteMultiplier.h
+++ NoteMultiplier.h
@@ -1,16 +1,16 @@
 #ifndef _NOTEMULTIPLIER_H_
 #define _NOTEMULTIPLIER_H_
 
-#include "Effect.h"
+#include "Track.h"
 
-class NoteMultiplier : public Effect {
+class NoteMultiplier : public Track {
  public:
-  NoteMultiplier() { }
+  NoteMultiplier() : Track(TrackType::EFFECT) { }
 
   std::string getElementName() const override { return "multiply"; }
   void loadParameters(const ParameterSource & input) override;
   void storeParameters(ParameterSource & output) const override;
-  std::unique_ptr<TrackState> playNote(ChannelConfiguration channel_config, int outSampleRate, float azimuth, float frequency, float velocity, float start_phase) const override;
+  std::unique_ptr<TrackState> playNote(const ChannelConfiguration & channel_config, float azimuth, float frequency, float velocity, float start_phase) const override;
 
 private:
   int unisons = 0;
