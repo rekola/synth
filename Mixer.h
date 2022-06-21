b1b495015f6d2afd2da672797fec0e561c499683
Mikael Rekola <mikael.rekola@gmail.com>
2022-06-21 03:05:28 +0300

    refactor

--- Mixer.h
+++ Mixer.h
@@ -1,21 +1,23 @@
 #ifndef _MIXER_H_
 #define _MIXER_H_
 
-#include "State.h"
 #include "SampleData.h"
 
-class Mixer : public State {
+class Mixer {
  public:
-  Mixer(short _out_channels, int _outSampleRate) : State(_outSampleRate), out_channels(_out_channels) { }
-
+  Mixer(short out_channels, int outSampleRate) : out_channels_(out_channels), outSampleRate_(outSampleRate) { }
+  virtual ~Mixer() { }
+  
   virtual void reset() = 0;
   virtual void accumulate(const SampleData & data, float volume = 1.0f) = 0;
   virtual SampleData encode(float master_volume) = 0;
 
-  short getOutChannels() const { return out_channels; }
+  short getOutChannels() const { return out_channels_; }
+  int getOutSampleRate() const { return outSampleRate_; }
   
 private:
-  short out_channels;
+  short out_channels_;
+  int outSampleRate_;
 };
 
 #endif
