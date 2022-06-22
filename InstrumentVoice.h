362a04d5590a3d1be5e144fadc8958cd2fe71c09
Mikael Rekola <mikael.rekola@gmail.com>
2022-06-22 17:30:43 +0300

    refactor

--- InstrumentVoice.h
+++ InstrumentVoice.h
@@ -5,31 +5,31 @@
 
 class InstrumentVoice : public TrackState {
  public:
-  InstrumentVoice(const ChannelConfiguration & _channel_config, float _azimuth)
-    : TrackState(_channel_config), azimuth(_azimuth) { }
+  InstrumentVoice(const ChannelConfiguration & channel_config, float azimuth)
+    : TrackState(channel_config), azimuth_(azimuth) { }
 
   void killNote() override {
     TrackState::killNote();
-
-    freq = 0.0f;
+    freq_ = 0.0f;
   }
+  
   void stopNote() override { killNote(); }
     
-  virtual void playNote(float _frequency, float velocity, float start_phase) {	
-    freq = _frequency;
+  virtual void playNote(float frequency, float velocity, float start_phase) {
+    freq_ = frequency;
     setGainDB(-gainToDecibels(1.0f / velocity));
-    sourceSamplePosition = start_phase * getChannelConfiguration().getAudioOutSampleRate();
+    sourceSamplePosition_ = start_phase * getChannelConfiguration().getAudioOutSampleRate();
   }
 
-  bool isPlaying() const override { return freq != 0.0f; }
+  bool isPlaying() const override { return freq_ != 0.0f; }
   bool isReleased() const { return false; }
     
   void setVolume(float volume) {
     setGainDB(gainToDecibels(volume));
   }
 
-  void setGainDB(float db) { noteGainDB = db; }
-  float getGainDB() const { return noteGainDB; }
+  void setGainDB(float db) { noteGainDB_ = db; }
+  float getGainDB() const { return noteGainDB_; }
 
   static inline float gainToDecibels(float gain) {
     return (gain <= .00001f ? -100.f : (float)(20.0 * log10(gain)));
@@ -40,21 +40,21 @@ class InstrumentVoice : public TrackState {
   }
   
 protected:
-  double getSourceSamplePosition() const { return sourceSamplePosition; }
+  double getSourceSamplePosition() const { return sourceSamplePosition_; }
 
   inline void stepForward(size_t frames) {
-    sourceSamplePosition += freq * frames;
+    sourceSamplePosition_ += freq_ * frames;
   }
 
-  float getFrequency() const { return freq; }
-  float getAzimuth() const { return azimuth; }
+  float getFrequency() const { return freq_; }
+  float getAzimuth() const { return azimuth_; }
   
-  double sourceSamplePosition = 0.0;
+  double sourceSamplePosition_ = 0.0;
 
 private:
-  float freq = 0.0f;
-  float noteGainDB = 0.0f;
-  float azimuth;
+  float freq_ = 0.0f;
+  float noteGainDB_ = 0.0f;
+  float azimuth_;
 };
 
 #endif
