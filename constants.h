eaa4ed6dbd5e3d256d5a96a7fb153753c0bdbcf8
Mikael Rekola <mikael.rekola@gmail.com>
2022-08-17 00:39:50 +0300

    refactor to proper C++

--- constants.h
+++ constants.h
@@ -1,9 +1,11 @@
 #ifndef _DEFAULTS_H_
 #define _DEFAULTS_H_
 
-// The lower this block size is the more accurate the effects are.
-// Increasing the value significantly lowers the CPU usage of the voice rendering.
-// If LFO affects the low-pass filter it can be hearable even as low as 8.
-#define RENDER_EFFECTSAMPLEBLOCK 64
+namespace constants {
+  // The lower this block size is the more accurate the effects are.
+  // Increasing the value significantly lowers the CPU usage of the voice rendering.
+  // If LFO affects the low-pass filter it can be hearable even as low as 8.
+  constexpr int RENDER_EFFECTSAMPLEBLOCK { 64 };
+};
 
 #endif
