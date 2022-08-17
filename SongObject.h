84a1c0d6f45a9c8e676c5c9e3d4338d7b5063669
Mikael Rekola <mikael.rekola@gmail.com>
2022-08-17 21:51:45 +0300

    move fields from Track to SongObject, remove unnecessary code

--- SongObject.h
+++ SongObject.h
@@ -1,12 +1,15 @@
 #ifndef _SONGOBJECT_H_
 #define _SONGOBJECT_H_
 
+#include "ParameterSource.h"
+
 #include <atomic>
 
 class SongObject {
  public:
   SongObject() : id_(getNextId()) { }
   SongObject(int id) : id_(id != -1 ? id : getNextId()) { }
+  SongObject(std::string name) : id_(getNextId()), name_(std::move(name)) { }
   virtual ~SongObject() { }
   
   int getId() const { return id_; }
@@ -15,11 +18,28 @@ class SongObject {
     return next_id.fetch_add(1);
   }
 
- protected:
-  void setId(int id) { id_ = id; }
+  virtual void loadParameters(const ParameterSource & input) {
+    id_ = input.getInt("id", -1);
+    setName(input.getText("name"));
+    setVolume(input.getFloat("volume", 1.0f));
+  }
+
+  virtual void storeParameters(ParameterSource & output) const {
+    output.set("id", id_);
+    if (!getName().empty()) output.set("name", getName());
+    output.set("volume", getVolume());
+  }
+
+  float getVolume() const { return volume_; }
+  void setVolume(float volume) { volume_ = volume; }
+
+  void setName(std::string name) { name_ = std::move(name); }
+  const std::string & getName() const { return name_; }
 
  private:
   int id_;
+  std::string name_;
+  float volume_ = 1.00f;
 
   static std::atomic<int> next_id;
 };
--- Track.h
+++ Track.h
@@ -5,14 +5,12 @@
 
 #include "SampleData.h"
 #include "TrackState.h"
-#include "ParameterSource.h"
 #include "TrackType.h"
 
 #include <string>
 #include <vector>
 #include <memory>
 #include <map>
-#include <atomic>
 
 class SongState;
 class TrackEventQueue;
@@ -20,7 +18,7 @@ class TrackEventQueue;
 class Track : public SongObject {
  public:
   Track(TrackType _type) : type(_type) { }
-  Track(TrackType _type, std::string _name) : type(_type), name(_name) { }
+  Track(TrackType _type, std::string _name) : SongObject(_name), type(_type) { }
   Track(int _id, TrackType _type) : SongObject(_id), type(_type) { }
   
   virtual SampleData render(int frames, SongState & song_state, const std::vector<std::unique_ptr<Track> > & instruments, TrackEventQueue & events);
@@ -29,19 +27,16 @@ class Track : public SongObject {
     return std::make_unique<TrackState>(config);
   }
 
-  virtual void loadParameters(const ParameterSource & input) {
-    setId(input.getInt("id", -1));
-    setName(input.getText("name"));
-    setVolume(input.getFloat("volume", 1.0f));
+  void loadParameters(const ParameterSource & input) override {
+    SongObject::loadParameters(input);
     setSolo(input.getBool("solo"));
     setMute(input.getBool("mute"));
   }
 
-  virtual void storeParameters(ParameterSource & output) const {
-    if (!getName().empty()) output.set("name", getName());
+  void storeParameters(ParameterSource & output) const override {
+    SongObject::storeParameters(output);
     if (isSolo()) output.set("solo", true);
     if (isMuted()) output.set("mute", true);
-    output.set("volume", getVolume());
   }
   
   virtual std::string getElementName() const = 0;
@@ -57,17 +52,11 @@ class Track : public SongObject {
 
   TrackType getType() const { return type; }
 
-  float getVolume() const { return volume; }
-  void setVolume(float _volume) { volume = _volume; }
-
   bool isSolo() const { return solo; }
   void setSolo(bool s) { solo = s; }
 
   bool isMuted() const { return mute; }
   void setMute(bool m) { mute = m; }
-
-  void setName(std::string _name) { name = _name; }
-  const std::string & getName() const { return name; }
 
   const Track & getChild(int i) const { return *(children[i]); }
   Track & getChild(int i) { return *(children[i]); }
@@ -77,24 +66,6 @@ class Track : public SongObject {
   std::vector<std::unique_ptr<Track> > & getChildren() { return children; }
   const std::vector<std::unique_ptr<Track> > & getChildren() const { return children; }
 
-  const Track * getChildById(int id) const {
-    if (id == getId()) return this;
-    for (auto & child : children) {
-      auto r = child->getChildById(id);
-      if (r) return r;
-    }
-    return nullptr;
-  }
-
-  Track * getChildById(int id) {
-    if (id == getId()) return this;
-    for (auto & child : children) {
-      auto r = child->getChildById(id);
-      if (r) return r;
-    }
-    return nullptr;
-  }
-
   bool showNoteColumn() const { return show_note_column; }
   bool showVelocityColumn() const { return show_velocity_column; }
   bool showEffectsColumn() const { return show_effects_column; }
@@ -102,9 +73,7 @@ class Track : public SongObject {
   
  private:
   TrackType type;
-  float volume = 1.00f;
   bool solo = false, mute = false;
-  std::string name;
   std::vector<std::unique_ptr<Track> > children;
 
   bool show_note_column = true;
