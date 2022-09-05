f000711d5f15ab563a1662b2f22ba937e61474a7
Mikael Rekola <mikael.rekola@gmail.com>
2022-09-05 15:19:47 +0300

    use string_view

--- FourCC.h
+++ FourCC.h
@@ -1,6 +1,8 @@
 #ifndef _FOURCC_H_
 #define _FOURCC_H_
 
+#include <string_view>
+
 class FourCC {
  public:
   FourCC() {
@@ -10,7 +12,8 @@ class FourCC {
     return value[0] == other.value[0] && value[1] == other.value[1] && value[2] == other.value[2] && value[3] == other.value[3];
   }
 
-  bool operator== (const char * other) {
+  bool operator== (std::string_view other) {
+    if (other.size() != 4) return false;
     return value[0] == other[0] && value[1] == other[1] && value[2] == other[2] && value[3] == other[3];
   }
 
