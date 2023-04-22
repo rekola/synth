d39473eee0acc7e7e42d905c8774a4cdf549f128
Mikael Rekola <mikael.rekola@gmail.com>
2023-04-22 03:18:41 +0300

    refactor

--- TrackInfo.h
+++ TrackInfo.h
@@ -3,17 +3,19 @@
 
 class TrackInfo {
 public:
-  TrackInfo(bool is_active = false, bool is_clipping = false)
-    : is_active_(is_active), is_clipping_(is_clipping) { }
+  TrackInfo(bool is_active = false, bool is_clipping = false, float meter_value = -1.0f)
+    : is_active_(is_active), is_clipping_(is_clipping), meter_value_(meter_value) { }
 
   bool isActive() const { return is_active_; }
   bool isRecording() const { return is_recording_; }
   bool isClipping() const { return is_clipping_; }
+  float getMeterValue() const { return meter_value_; }
   
  private:
-  bool is_active_ = false;
-  bool is_recording_ = false;
-  bool is_clipping_ = false;
+  bool is_active_;
+  bool is_recording_;
+  bool is_clipping_;
+  float meter_value_;
 };
 
 #endif
